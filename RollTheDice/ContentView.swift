42b09906cb617c5a58a6329426745a737e64a6a9
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-21 10:38:39 -0400

    Initial Commit

---
+++ RollTheDice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RollTheDice
+//
+//  Created by Helwan Mandé on 2020-08-21.
+//  Copyright © 2020 Helwan Mandé. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
